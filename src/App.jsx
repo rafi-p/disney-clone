bc0c697c394046f61ed3a60ca2caed557af039a5
Rafi <mcintoshhd@Mcintoshs-Air.gateway>
2024-03-13 21:08:57 +1100

    production house UI, add video for background, absolute relative setting with z css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header'
+import ProductionHouse from './components/ProductionHouse'
 import Slider from './components/Slider'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="">
       <Header />
       <Slider />
+      <ProductionHouse />
     </div>
   )
 }
